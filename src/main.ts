0580f7cfea222f11d4db5efe9d4079730822220f
ZHANGYU <hey@zhangyu.dev>
2024-05-30 17:02:42 +0800

    rapier3d

--- src/main.ts
+++ src/main.ts
@@ -1,392 +1,375 @@
+import './style.css'
+
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-
+import { Timer } from 'three/addons/misc/Timer.js'
+import { type RigidBody } from '@dimforge/rapier3d'
 import { Pane } from 'tweakpane'
 import * as Essentials from '@tweakpane/plugin-essentials'
 
-import PhysicalWorker from './worker?worker'
+import('@dimforge/rapier3d').then((RAPIER) => {
+  const scene = new THREE.Scene()
 
-import type { MainEvent, SyncData, RemoveData } from './interface'
-
-import './style.css'
-
-const worker = new PhysicalWorker()
-
-const scene = new THREE.Scene()
-
-const planeSize = {
-  width: 25,
-  height: 25,
-}
-
-const cubeSize = {
-  width: 1,
-  height: 1,
-  depth: 1,
-}
-
-const debug = {
-  shadowCameraHelperEnable: false,
-}
-
-const initialInfo = {
-  x: 0,
-  y: 15,
-  z: 0,
-  force: -20,
-  flow: 3,
-}
-
-const interval = {
-  time: 150,
-}
-
-/**
- * Mesh
- */
-const cubeGeometry = new THREE.BoxGeometry(
-  cubeSize.width,
-  cubeSize.height,
-  cubeSize.depth,
-)
-
-const cubeMaterials = [
-  new THREE.MeshLambertMaterial({ color: 0xf87171 }),
-  new THREE.MeshLambertMaterial({ color: 0xfde047 }),
-  new THREE.MeshLambertMaterial({ color: 0xa78bfa }),
-  new THREE.MeshLambertMaterial({ color: 0xf472b6 }),
-  new THREE.MeshLambertMaterial({ color: 0x34d399 }),
-  new THREE.MeshLambertMaterial({ color: 0x8b5cf6 }),
-]
-
-const planeMesh = new THREE.Mesh(
-  new THREE.PlaneGeometry(planeSize.width, planeSize.height),
-  new THREE.MeshLambertMaterial({
-    color: 0x60a5fa,
-    side: THREE.DoubleSide,
-  }),
-)
-planeMesh.rotation.x = -Math.PI / 2
-planeMesh.receiveShadow = true
-
-scene.add(planeMesh)
-
-/**
- * Light
- */
-const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-scene.add(ambientLight)
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
-directionalLight.position.set(0, 15, 5)
-
-directionalLight.castShadow = true
-directionalLight.shadow.camera.near = 0.5
-directionalLight.shadow.camera.far = 20
-directionalLight.shadow.camera.top = 15
-directionalLight.shadow.camera.right = 15
-directionalLight.shadow.camera.bottom = -15
-directionalLight.shadow.camera.left = -15
-
-scene.add(directionalLight)
-
-/**
- * Renderer
- */
-
-const renderer = new THREE.WebGLRenderer({
-  alpha: true,
-  antialias: true,
-})
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.BasicShadowMap
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(innerWidth, innerHeight)
-
-document.body.appendChild(renderer.domElement)
-
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight)
-
-camera.position.x = -20
-camera.position.y = 20
-camera.position.z = -40
-
-scene.add(camera)
-
-const orbitControls = new OrbitControls(camera, renderer.domElement)
-orbitControls.enableDamping = true
-orbitControls.enablePan = false
-
-const directionalLightCameraHelper = new THREE.CameraHelper(
-  directionalLight.shadow.camera,
-)
-
-/**
- * Worker
- */
-const bodyMeshMap = new Map<number, THREE.Mesh>()
-let currentId = 1
-
-const isSyncData = (data: unknown): data is MainEvent<SyncData> =>
-  (data as MainEvent<unknown>).type === 'sync'
-
-const isRemoveData = (data: unknown): data is MainEvent<RemoveData> =>
-  (data as MainEvent<unknown>).type === 'remove'
-
-const requestSynFromWorker = () => {
-  worker.postMessage({
-    type: 'step',
-  })
-}
-
-worker.postMessage({
-  type: 'init',
-  payload: { plane: planeSize, cube: cubeSize },
-})
-
-worker.addEventListener('message', (event) => {
-  const { data } = event
-  if (isRemoveData(data)) {
-    removeCubeMesh(data.payload)
-  } else if (isSyncData(event.data)) {
-    syncMesh(data.payload)
+  const planeSize = {
+    width: 25,
+    height: 25,
   }
-})
 
-/**
- * Functions
- */
-const removeCubeMesh = ({ id }: RemoveData) => {
-  for (const value of id) {
-    const mesh = bodyMeshMap.get(value)
-    if (mesh) {
-      scene.remove(mesh)
-    }
+  const cubeSize = {
+    width: 1,
+    height: 1,
+    depth: 1,
   }
-}
 
-const syncMesh = ({ data }: SyncData) => {
-  const len = data.length / 8
-  for (let i = 0; i < len; i++) {
-    const index = i * 8
-    const id = data[index]
-
-    if (id === 0) {
-      break
-    }
-
-    const position = new THREE.Vector3(
-      data[index + 1],
-      data[index + 2],
-      data[index + 3],
-    )
-    const quaternion = new THREE.Quaternion(
-      data[index + 4],
-      data[index + 5],
-      data[index + 6],
-      data[index + 7],
-    )
-
-    const cubeMesh = bodyMeshMap.get(id)
-
-    if (cubeMesh) {
-      cubeMesh.position.copy(position)
-      cubeMesh.quaternion.copy(quaternion)
-    }
+  const debug = {
+    shadowCameraHelperEnable: false,
   }
-}
 
-const createCubeMeshWithPhysics = (position: THREE.Vector3) => {
-  const id = currentId++
+  const initialInfo = {
+    x: 0,
+    y: 15,
+    z: 0,
+    force: -20,
+    flow: 1,
+  }
 
-  worker.postMessage({
-    type: 'add',
-    payload: {
-      id,
-      force: initialInfo.force * Math.max(initialInfo.flow / 3, 1),
-      position: [position.x, position.y, position.z],
-    },
-  })
+  const interval = {
+    time: 1000,
+  }
 
-  const cubeMesh = new THREE.Mesh(
-    cubeGeometry,
-    cubeMaterials[Math.floor(Math.random() * cubeMaterials.length)],
+  /**
+   * Mesh
+   */
+  const cubeGeometry = new THREE.BoxGeometry(
+    cubeSize.width,
+    cubeSize.height,
+    cubeSize.depth,
   )
 
-  cubeMesh.position.copy(position)
-  cubeMesh.castShadow = true
+  const cubeMaterials = [
+    new THREE.MeshLambertMaterial({ color: 0xf87171 }),
+    new THREE.MeshLambertMaterial({ color: 0xfde047 }),
+    new THREE.MeshLambertMaterial({ color: 0xa78bfa }),
+    new THREE.MeshLambertMaterial({ color: 0xf472b6 }),
+    new THREE.MeshLambertMaterial({ color: 0x34d399 }),
+    new THREE.MeshLambertMaterial({ color: 0x8b5cf6 }),
+  ]
 
-  scene.add(cubeMesh)
+  const planeMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(planeSize.width, planeSize.height),
+    new THREE.MeshLambertMaterial({
+      color: 0x60a5fa,
+      side: THREE.DoubleSide,
+    }),
+  )
+  planeMesh.rotation.x = -Math.PI / 2
+  planeMesh.receiveShadow = true
 
-  bodyMeshMap.set(id, cubeMesh)
-}
+  scene.add(planeMesh)
 
-/**
- * rAF
- */
-requestSynFromWorker()
+  /**
+   * Light
+   */
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+  scene.add(ambientLight)
 
-const tick = () => {
-  fpsGraph.begin()
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
+  directionalLight.position.set(0, 15, 5)
+
+  directionalLight.castShadow = true
+  directionalLight.shadow.camera.near = 0.5
+  directionalLight.shadow.camera.far = 20
+  directionalLight.shadow.camera.top = 15
+  directionalLight.shadow.camera.right = 15
+  directionalLight.shadow.camera.bottom = -15
+  directionalLight.shadow.camera.left = -15
+
+  scene.add(directionalLight)
+
+  /**
+   * Renderer
+   */
+
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+  })
+  renderer.shadowMap.enabled = true
+  renderer.shadowMap.type = THREE.BasicShadowMap
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(innerWidth, innerHeight)
+
+  document.body.appendChild(renderer.domElement)
+
+  /**
+   * Camera
+   */
+  const camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight)
+
+  camera.position.x = -20
+  camera.position.y = 20
+  camera.position.z = -40
+
+  scene.add(camera)
+
+  const orbitControls = new OrbitControls(camera, renderer.domElement)
+  orbitControls.enableDamping = true
+  orbitControls.enablePan = false
+
+  const directionalLightCameraHelper = new THREE.CameraHelper(
+    directionalLight.shadow.camera,
+  )
+
+  /**
+   * Physics
+   */
+  const world = new RAPIER.World({ x: 0, y: -9.82, z: 0 })
+
+  const groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.0, 0, 10.0)
+  world.createCollider(groundColliderDesc)
+
+  const cubeColliderDesc = RAPIER.ColliderDesc.cuboid(
+    cubeSize.width / 2,
+    cubeSize.height / 2,
+    cubeSize.depth / 2,
+  )
+
+  const cubeBodies = new Set<RigidBody>()
+  const bodyMeshMap = new WeakMap<RigidBody, THREE.Mesh>()
+
+  /**
+   * Functions
+   */
+  const removeCubeBody = (cubeBody: RigidBody) => {
+    if (cubeBody) {
+      cubeBodies.delete(cubeBody)
+      world.removeRigidBody(cubeBody)
+
+      const mesh = bodyMeshMap.get(cubeBody)
+      if (mesh) {
+        scene.remove(mesh)
+        bodyMeshMap.delete(cubeBody)
+      }
+    }
+  }
+
+  const syncMeshWithBody = () => {
+    cubeBodies.forEach((cubeBody) => {
+      const position = cubeBody.translation()
+      const rotation = cubeBody.rotation()
+
+      if (position.y < -50 || cubeBody.isSleeping()) {
+        removeCubeBody(cubeBody)
+        return
+      }
+
+      const cubeMesh = bodyMeshMap.get(cubeBody)
+
+      if (cubeMesh) {
+        cubeMesh.position.copy(position)
+        cubeMesh.quaternion.copy(rotation)
+      }
+    })
+  }
+
+  const createCubeMeshWithPhysics = (position: THREE.Vector3) => {
+    const cubeBodyDesc = RAPIER.RigidBodyDesc.dynamic().setTranslation(
+      position.x,
+      position.y,
+      position.z,
+    )
+
+    const cubeBody = world.createRigidBody(cubeBodyDesc)
+
+    world.createCollider(cubeColliderDesc, cubeBody)
+
+    cubeBody.setLinvel({ x: 0, y: initialInfo.force, z: 0 }, true)
+
+    cubeBodies.add(cubeBody)
+
+    const cubeMesh = new THREE.Mesh(
+      cubeGeometry,
+      cubeMaterials[Math.floor(Math.random() * cubeMaterials.length)],
+    )
+
+    cubeMesh.position.copy(position)
+    cubeMesh.castShadow = true
+
+    scene.add(cubeMesh)
+
+    bodyMeshMap.set(cubeBody, cubeMesh)
+  }
+
+  /**
+   * rAF
+   */
+  const timer = new Timer()
+  const tick = (timestamp: number) => {
+    fpsGraph.begin()
+
+    requestAnimationFrame(tick)
+
+    timer.update(timestamp)
+
+    world.step()
+
+    syncMeshWithBody()
+
+    orbitControls.update()
+
+    renderer.render(scene, camera)
+
+    fpsGraph.end()
+  }
 
   requestAnimationFrame(tick)
 
-  orbitControls.update()
+  /**
+   * Create Cube
+   */
+  let id: number
 
-  renderer.render(scene, camera)
+  const createCube = () => {
+    clearInterval(id)
+    id = setInterval(() => {
+      for (let i = 0; i < initialInfo.flow; i++) {
+        const angle = Math.random() * 2 * Math.PI
+        const r = (initialInfo.flow + 1) * Math.sqrt(Math.random())
+        const offsetX = r * Math.cos(angle)
+        const offsetZ = r * Math.sin(angle)
 
-  fpsGraph.end()
-}
+        createCubeMeshWithPhysics(
+          new THREE.Vector3(
+            initialInfo.x + offsetX,
+            initialInfo.y + Math.random() * (2 + initialInfo.flow),
+            initialInfo.z + offsetZ,
+          ),
+        )
+      }
+    }, interval.time)
+  }
 
-requestAnimationFrame(tick)
-/**
- * Create Cube
- */
-let id: number
+  createCube()
 
-const createCube = () => {
-  clearInterval(id)
-  id = setInterval(() => {
-    for (let i = 0; i < initialInfo.flow; i++) {
-      const angle = Math.random() * 2 * Math.PI
-      const r = (initialInfo.flow + 1) * Math.sqrt(Math.random())
-      const offsetX = r * Math.cos(angle)
-      const offsetZ = r * Math.sin(angle)
+  /**
+   * Tweakpane
+   */
+  const pane = new Pane()
+  pane.registerPlugin(Essentials)
 
-      createCubeMeshWithPhysics(
-        new THREE.Vector3(
-          initialInfo.x + offsetX,
-          initialInfo.y + Math.random() * (2 + initialInfo.flow),
-          initialInfo.z + offsetZ,
-        ),
-      )
-    }
-  }, interval.time)
-}
+  pane
+    .addBinding(interval, 'time', {
+      label: 'Create Interval',
+      min: 0,
+      max: 1000,
+      step: 50,
+    })
+    .on('change', (changed) => {
+      if (changed) {
+        createCube()
+      }
+    })
 
-createCube()
-
-/**
- * Tweakpane
- */
-const pane = new Pane()
-pane.registerPlugin(Essentials)
-
-pane
-  .addBinding(interval, 'time', {
-    label: 'Create Interval',
-    min: 0,
-    max: 1000,
-    step: 50,
-  })
-  .on('change', (changed) => {
-    if (changed) {
-      createCube()
-    }
-  })
-
-pane.addBinding(initialInfo, 'flow', {
-  label: 'Create Flow',
-  min: 1,
-  max: 10,
-  step: 1,
-})
-
-pane.addBinding({ initialInfo }, 'initialInfo', {
-  label: 'Initial Position',
-  x: {
-    min: -10,
+  pane.addBinding(initialInfo, 'flow', {
+    label: 'Create Flow',
+    min: 1,
     max: 10,
     step: 1,
-  },
-  y: {
-    min: 10,
-    max: 50,
-    step: 1,
-  },
-  z: {
-    min: -10,
-    max: 10,
-    step: 1,
-  },
-})
+  })
 
-pane.addBinding(initialInfo, 'force', {
-  label: 'Initial Force',
-  min: -50,
-  max: 50,
-  step: 1,
-})
-
-const shadowFolder = pane.addFolder({
-  title: 'Shadow',
-  expanded: false,
-})
-
-shadowFolder.addBinding(renderer.shadowMap, 'enabled', {
-  label: 'Enable',
-})
-
-shadowFolder.addBinding(renderer.shadowMap, 'type', {
-  label: 'Type',
-  options: {
-    BasicShadowMap: THREE.BasicShadowMap,
-    PCFShadowMap: THREE.PCFShadowMap,
-    PCFSoftShadowMap: THREE.PCFSoftShadowMap,
-  },
-})
-
-shadowFolder
-  .addBinding({ width: directionalLight.shadow.mapSize.width }, 'width', {
-    label: 'Map Size',
-    options: {
-      128: 128,
-      256: 256,
-      512: 512,
-      1024: 1024,
-      2048: 2048,
-      4096: 4096,
+  pane.addBinding({ initialInfo }, 'initialInfo', {
+    label: 'Initial Position',
+    x: {
+      min: -10,
+      max: 10,
+      step: 1,
+    },
+    y: {
+      min: 10,
+      max: 50,
+      step: 1,
+    },
+    z: {
+      min: -10,
+      max: 10,
+      step: 1,
     },
   })
-  .on('change', (changed) => {
-    if (changed) {
-      const value = changed.value
-      directionalLight.shadow.mapSize.width = value
-      directionalLight.shadow.mapSize.height = value
-      directionalLight.shadow.map?.dispose()
-      directionalLight.shadow.map = null
-    }
+
+  pane.addBinding(initialInfo, 'force', {
+    label: 'Initial Force',
+    min: -50,
+    max: 50,
+    step: 1,
   })
 
-const debugFolder = pane.addFolder({
-  title: 'Debug',
-  expanded: false,
-})
-
-debugFolder
-  .addBinding(debug, 'shadowCameraHelperEnable', {
-    label: 'Shadow Camera Helper Enable',
+  const shadowFolder = pane.addFolder({
+    title: 'Shadow',
+    expanded: false,
   })
-  .on('change', (changed) => {
-    if (changed) {
-      if (debug.shadowCameraHelperEnable) {
-        scene.add(directionalLightCameraHelper)
-      } else {
-        scene.remove(directionalLightCameraHelper)
+
+  shadowFolder.addBinding(renderer.shadowMap, 'enabled', {
+    label: 'Enable',
+  })
+
+  shadowFolder.addBinding(renderer.shadowMap, 'type', {
+    label: 'Type',
+    options: {
+      BasicShadowMap: THREE.BasicShadowMap,
+      PCFShadowMap: THREE.PCFShadowMap,
+      PCFSoftShadowMap: THREE.PCFSoftShadowMap,
+    },
+  })
+
+  shadowFolder
+    .addBinding({ width: directionalLight.shadow.mapSize.width }, 'width', {
+      label: 'Map Size',
+      options: {
+        128: 128,
+        256: 256,
+        512: 512,
+        1024: 1024,
+        2048: 2048,
+        4096: 4096,
+      },
+    })
+    .on('change', (changed) => {
+      if (changed) {
+        const value = changed.value
+        directionalLight.shadow.mapSize.width = value
+        directionalLight.shadow.mapSize.height = value
+        directionalLight.shadow.map?.dispose()
+        directionalLight.shadow.map = null
       }
-    }
+    })
+
+  const debugFolder = pane.addFolder({
+    title: 'Debug',
+    expanded: false,
   })
 
-pane.addBlade({
-  view: 'separator',
+  debugFolder
+    .addBinding(debug, 'shadowCameraHelperEnable', {
+      label: 'Shadow Camera Helper Enable',
+    })
+    .on('change', (changed) => {
+      if (changed) {
+        if (debug.shadowCameraHelperEnable) {
+          scene.add(directionalLightCameraHelper)
+        } else {
+          scene.remove(directionalLightCameraHelper)
+        }
+      }
+    })
+
+  pane.addBlade({
+    view: 'separator',
+  })
+
+  const fpsGraph = pane.addBlade({
+    view: 'fpsgraph',
+
+    label: 'fpsgraph',
+    rows: 2,
+  }) as Essentials.FpsGraphBladeApi
 })
-
-const fpsGraph = pane.addBlade({
-  view: 'fpsgraph',
-
-  label: 'fpsgraph',
-  rows: 2,
-}) as Essentials.FpsGraphBladeApi
